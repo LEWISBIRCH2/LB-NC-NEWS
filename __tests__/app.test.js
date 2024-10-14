2ca5fb60c11b78529b800398182e01e47b04f28a
Lewis <lewisbirch2@googlemail.com>
2024-10-14 12:13:32 +0100

    Completed T3. Adding API endpoint list

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const app = require("../app");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const db = require("../db/connection");
+const endpoints = require('../endpoints.json')
 
 beforeEach(() => {
   return seed(data);
@@ -25,3 +26,14 @@ describe("Topics endpoint", () => {
       });
   });
 });
+
+describe("API endpoint", () => {
+  test("GET:200 - Returns an array of potential valid endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpoints)
+        });
+      });
+  });
