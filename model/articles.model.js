993c851c08f8b6172c048c34fade89d957cb6e9e
Lewis <lewisbirch2@googlemail.com>
2024-10-15 12:25:09 +0100

    Completed T7 - Posting a comment. Refactored previous tests to allow empty arrays to pass specific tests.

--- model/articles.model.js
+++ model/articles.model.js
@@ -1,3 +1,4 @@
+const { post } = require("superagent");
 const db = require("../db/connection");
 
 function fetchAllArticles(request, response) {
@@ -25,7 +26,7 @@ function fetchArticle(artNum) {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [artNum])
     .then((result) => {
-      if (result.rowCount === 0) {
+      if (result.rows.length === 0) {
         return Promise.reject({ status: 404, message: "Article Not Found" });
       }
       return result.rows;
@@ -39,11 +40,24 @@ function fetchArticleComments(artComNum) {
       [artComNum]
     )
     .then((result) => {
-      if (result.rowCount === 0) {
-        return Promise.reject({ status: 404, message: "Article Not Found" });
-      } else {
-        return result.rows;
+            return result.rows;
       }
+    )}
+    
+function publishArticleComment(postAuthor, postBody, postNum) {
+  return db
+    .query(
+      "INSERT INTO comments (author,body,article_id) VALUES ($1, $2, $3) RETURNING *;",
+      [postAuthor, postBody, postNum]
+    )
+    .then((result) => {
+      return result.rows[0];
     });
 }
-module.exports = { fetchArticle, fetchAllArticles, fetchArticleComments };
+
+module.exports = {
+  fetchArticle,
+  fetchAllArticles,
+  fetchArticleComments,
+  publishArticleComment,
+};
